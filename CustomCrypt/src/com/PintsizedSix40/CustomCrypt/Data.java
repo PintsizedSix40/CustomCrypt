647d51c1e4cb02e96e5723a65f69e1e7f40a76a3
PintsizedSix40 <PintsizedSix40@yahoo.com>
2019-04-23 20:32:47 -0700

    Finished Custom Algos

---
+++ CustomCrypt/src/com/PintsizedSix40/CustomCrypt/Data.java
@@ -0,0 +1,58 @@
+package com.PintsizedSix40.CustomCrypt;
+
+import net.objecthunter.exp4j.operator.Operator;
+
+public class Data {
+
+	public static String prevOutput = "0000";
+	public static int iteration = 1;
+	
+	public static Operator a = new Operator("$", 2, true, Operator.PRECEDENCE_DIVISION-1) {
+		@Override
+        public double apply(double... values) {
+		return values[1]+values[0];
+			
+		}
+		
+	};
+	
+	public static Operator s = new Operator("#", 2, true, Operator.PRECEDENCE_MULTIPLICATION-1) {
+		@Override
+        public double apply(double... values) {
+			if(values[1] > values[0]) {
+		return values[1]-values[0];
+			}
+			return values[0]-values[1];
+		}
+		
+	};
+	
+	public static Operator d = new Operator(">", 2, true, Operator.PRECEDENCE_ADDITION-1) {
+		@Override
+        public double apply(double... values) {
+			if(values[1] > values[0]) {
+		return values[1]/values[0];
+			}
+			return values[0]/values[1];
+			
+		}
+		
+	};
+	public static Operator m = new Operator("<", 2, true, Operator.PRECEDENCE_SUBTRACTION-1) {
+		@Override
+        public double apply(double... values) {
+		return values[1]*values[0];
+			
+		}
+		
+	};
+	
+	public static Operator p = new Operator("&", 2, true, Operator.PRECEDENCE_POWER) {
+		@Override
+        public double apply(double... values) {
+			return Math.pow(Math.E, Math.log(values[1])/values[0]);
+			
+		}
+		
+	};
+}
