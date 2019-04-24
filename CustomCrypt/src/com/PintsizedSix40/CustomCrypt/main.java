1bb40bc87e567af72f86c2b87bc6ba73e9f3fd1a
PintsizedSix40 <PintsizedSix40@yahoo.com>
2019-04-24 15:28:12 -0700

    GUI works and only needs save/load features. Algo implementation is in progress. PASS and CUSTOM work.

--- CustomCrypt/src/com/PintsizedSix40/CustomCrypt/main.java
+++ CustomCrypt/src/com/PintsizedSix40/CustomCrypt/main.java
@@ -7,13 +7,6 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 public class main {
 
 	public static void main(String[] args) {
-		String[] k = new String[3];
-		k[0] = "12";
-		k[1] = "16";
-		k[2] = "6";
-		Evaluate e = new Evaluate("1234", k, "%k1%-%i%*%k2%+%p%");
-		System.out.println((int)(char)Character.toChars(Math.abs(e.Evaluate()))[0]);
-		Evaluate e2 = new Evaluate("19732", k, "%k1%-%i%*%k2%+%p%");
-		System.out.println(e2.DeEvaluate());
+		FrameHandler.setup();
 			}
 }
